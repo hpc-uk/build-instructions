d8f56fa7c9d82b029e87ad21f772332e5d7ee16e
Mark Filipiak <m.filipiak@epcc.ed.ac.uk>
2019-04-05 16:11:05 +0100

    Tidy Archer Libxc install script

--- Libxc/4.3.4_ARCHER_IvyBridge_gcc6/install.bash
+++ Libxc/4.3.4_ARCHER_IvyBridge_gcc6/install.bash
@@ -4,11 +4,6 @@ module switch PrgEnv-$(echo $PE_ENV | tr '[:upper:]' '[:lower:]') PrgEnv-gnu
 export CC=cc
 export FC=ftn
 export CRAYPE_LINK_TYPE=dynamic
-# The prefix will be something like
-# /work/y07/y07/cse/libxc/4.3.4_build1/GNU, since this script should
-# be run in something like
-# /work/y07/y07/cse/libxc/4.3.4_build1/GNU/build.
-prefix=$(readlink -f $PWD/..)
 (
     cd libxc-4.3.4
     make install &> install.log
