5437394ae8d29a27c711d150065f174d515a529b
Kevin Stratford <kevin@epcc.ed.ac.uk>
2021-06-24 08:03:56 +0100

    Add ARCHER2 openfoam.com/v2012

---
+++ apps/OpenFOAM/ARCHER2-OpenFOAM-v2012/site/compile.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# To be run from FOAM_INST_DIR
+# I make the dependencies first.
+
+source ./site/modules.sh
+
+cd OpenFOAM-v2012
+source ./etc/bashrc
+
+
+./Allwmake -j 16 dep
+./Allwmake -j 16 
