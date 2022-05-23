ba405a78f158440fbeef43a929b5f4020192b047
Kevin Stratford <kevin@epcc.ed.ac.uk>
2022-05-23 08:41:30 +0100

    Fix incorrect libxc substitution

--- apps/GPAW/ARCHER2_gpaw22.1.0/build_gpaw.sh
+++ apps/GPAW/ARCHER2_gpaw22.1.0/build_gpaw.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
+set -e
+
 # GPAW
+# This location (my_prefix) is expected to hold the libxc installation
 
 export my_prefix=$(pwd)
+export libxc_prefix=${my_prefix}
 
 module load PrgEnv-gnu
 module load cray-python
@@ -12,7 +16,7 @@ git clone -b 22.1.0 https://gitlab.com/gpaw/gpaw.git
 cd gpaw
 
 cp ../siteconfig.py .
-sed -i "s#LIBXC_PATH#${prefix}#" siteconfig.py
+sed -i "s#LIBXC_PATH#${libxc_prefix}#" siteconfig.py
 
 python setup.py build_ext
 python setup.py install --prefix=${my_prefix}
