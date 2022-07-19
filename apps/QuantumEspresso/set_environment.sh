c0ba2f38c9ec9eded664cd520d73d0c339c32677
jodaelliott <jodaelliott@gmail.com>
2022-07-19 10:28:09 +0100

    QuantumEspresso 7.0 compilation recipe

---
+++ apps/QuantumEspresso/set_environment.sh
@@ -0,0 +1,12 @@
+module load PrgEnv-gnu
+module load cray-fftw cray-hdf5-parallel
+
+export CC=cc
+export FC=ftn
+export F77=ftn
+export F90=ftn
+
+export BLAS_LIBS=" "
+export LAPACK_LIBS=" "
+export SCALAPACK_LIBS=" "
+export FFT_LIBS=" "
